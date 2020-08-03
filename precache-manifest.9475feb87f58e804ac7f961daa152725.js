2452b8c0328c0492d4517685df2cc2e08b31d377
OscarBazanez <41228346+OscarBazanez@users.noreply.github.com>
2020-08-03 14:21:43 -0500

    Add files via upload

---
+++ precache-manifest.9475feb87f58e804ac7f961daa152725.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43227179c6cc1ad7afb573a8d37a5f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f6f8e6f107dd2ec4c9",
+    "url": "/static/js/2.633a5703.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.633a5703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ff466262e8daafd8b9",
+    "url": "/static/js/main.0074191e.chunk.js"
+  },
+  {
+    "revision": "7d8553486433103278de",
+    "url": "/static/js/runtime-main.29686f39.js"
+  }
+]);
